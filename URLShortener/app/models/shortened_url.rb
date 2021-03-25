e3d8864dde817ad90a3f4eee0ec7d7e7dda84235
aturi <angelaturi99@gmail.com>
2021-03-25 17:32:04 -0400

    Add Visit

--- URLShortener/app/models/shortened_url.rb
+++ URLShortener/app/models/shortened_url.rb
@@ -7,21 +7,25 @@ include SecureRandom
     def self.random_code
         short = SecureRandom.urlsafe_base64
         loop do 
-            return short unless ShortenedUrl.exists?(short_urls: short )
+            return short unless ShortenedUrl.exists?(short_url: short )
             # short = SecureRandom.urlsafe_base64
         end
 
         return short
     end
 
-    def self.create_short_urls(user_obj, long_url)
-        self.new(long_url, ShortenedUrl.random_code, user_obj.id)
+    def self.create_short_url(user, long_url)
+        ShortenedUrl.create!(
+            long_url: long_url,
+            short_url: ShortenedUrl.random_code,
+            user_id: user.id)
     end
 
     belongs_to :submitter,
         primary_key: :id,
         foreign_key: :user_id,
         class_name: :User
+        
 
     
 end
---
+++ URLShortener/app/models/visit.rb
@@ -0,0 +1,22 @@
+class Visit < ApplicationRecord
+    
+
+    
+    def self.record_visit!(user, shortened_url)
+        Visit.create!(
+            user_id: user.id,
+            shortened_url_id: shortened_url.id
+        )
+    end
+
+    belongs_to :shortened_url,
+        primary_key: :id,
+        foreign_key: :shortened_url_id,
+        class_name: :ShortenedUrl
+
+    belongs_to :visitor,
+        primary_key: :id,
+        foreign_key: :user_id,
+        class_name: :User
+    
+end
